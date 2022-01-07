01009eafc529cadb71e119b018cc2add9a7b01a2
hugoresende27 <Hugoresende27@gmail.com>
2022-01-07 11:18:40 +0000

    a

---
+++ aula3/connect.php
@@ -0,0 +1,11 @@
+
+<?php
+
+    $servidor = "localhost";
+    $user = "root";
+    $password =  "";
+    $bd = "testes";
+
+    //mysqli extenção incluida no php para ligar À BD
+    $ligacao = mysqli_connect($servidor,$user,$password,$bd);
+
--- aula3/null.php
+++ aula3/null.php
@@ -1,17 +1,18 @@
 <!DOCTYPE html>
-<html lang="pt">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Recurso</title>
+    <title>Var NULL</title>
 </head>
 <body>
+
 <?php
 
-//variavel do tipo recurso, ligação a uma base de dados
-    include "connect.php";
-    
+    //NULL significa uma var vazia
+    $vazio = NULL;
+    echo $vazio;
 
 ?>
     
