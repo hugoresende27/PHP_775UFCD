1d1307397a4a711b1c63dd1e1b3dc2abde340db4
hugoresende27 <Hugoresende27@gmail.com>
2022-01-24 10:57:05 +0000

    a

--- aula7/mostraAula.php
+++ aula7/mostraAula.php
@@ -19,11 +19,27 @@
     //testar se há dados na tabela
     if (mysqli_num_rows($res) > 0 ){
         //mostrar dados da tabela
+?>
+    <table border="1">
+        <tr>
+            <th>Nome</th>
+            <th>Email</th>
+            <th>Telefone</th>       
+            <th>Gênero</th>
+            <th>Mensagem</th>
+        </tr>
+    
+<?php
         while ($linha = mysqli_fetch_assoc($res)){
-            echo "<pre>";
-            echo $linha['nome'];
-            print_r($linha).PHP_EOL;
-            echo "</pre>";
+            // echo "<pre>";
+            // echo $linha['nome'];
+            // print_r($linha).PHP_EOL;
+            // echo "</pre>";
+            echo "<tr><td>".$linha['nome']."</td>";
+            echo "<td>".$linha['email']."</td>";
+            echo "<td>".$linha['telefone']."</td>";
+            echo "<td>".$linha['genero']."</td>";
+            echo "<td>".$linha['mensagem']."</td></tr>";
         }
 
     } else {
@@ -31,8 +47,12 @@
         echo "0 resultados...";
     }
 
-
 ?>
+
+        
+    </table>
+
         <a class ="negrito" href="formularios2.php">Voltar</a>
 </body>
+
 </html>
